29e8759d6df5798ad4c71b67ce0cb75241e72476
pinguo-liyanjun <liyanjun@camera360.com>
2015-07-03 09:23:54 +0800

    Initial Commit

---
+++ PictureDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PictureDemo
+//
+//  Created by C360_liyanjun on 15/7/3.
+//  Copyright (c) 2015年 C360_liyanjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
